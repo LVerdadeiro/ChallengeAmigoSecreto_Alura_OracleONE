44c92f45f676e13a502d9d9bd2fcd58023d8430b
Lucas Verdadeiro C. <112838171+LVerdadeiro@users.noreply.github.com>
2025-03-09 09:18:05 -0300

    Implementação da funcionalidade de adicionar amigos
    Funcionalidade de adição de nomes à lista de amigos a serem sorteados.

--- challenge-amigo-secreto_ONE/app.js
+++ challenge-amigo-secreto_ONE/app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+let input_amigo = document.getElementById("amigo");
+let lista_de_amigos = document.getElementById("listaAmigos");
+
+function adicionarAmigo() {
+    let nome = input_amigo.value;
+
+    if (nome && !amigos.includes(nome)) {
+        amigos.push(nome);
+
+        let elemento_lista = document.createElement("li");
+        elemento_lista.textContent = nome;
+        lista_de_amigos.appendChild(elemento_lista);
+
+        input_amigo.value = "";
+    } else 
+        alert("Nome inválido ou já adicionado!");
+    
+}
